fa64b317ebef23f282119d4b7bec3b14d2d88b46
kravage <kostyash2009@mail.ru>
2025-02-19 17:25:34 +0300

    working 1st task

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import sys
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel
+from map_func import scale
+from os import remove
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         label = QLabel(self)
-        pixmap = QPixmap('')
+
+        self.img = scale()
+
+        pixmap = QPixmap(self.img)
         label.setPixmap(pixmap)
         self.setCentralWidget(label)
         self.resize(pixmap.width(), pixmap.height())
+
+    def closeEvent(self):
+        remove(self.img)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- map_func.py
+++ map_func.py
@@ -4,12 +4,13 @@ from io import BytesIO
 
 
 def scale():
-    map_params = {'spn': ','.join(['123.21312', '123.43623']),
-                  'll': ','.join(['122.1256', '24.2412']),
+    map_params = {'spn': ','.join(['0.002', '0.002']),
+                  'll': ','.join(['37.530887', '55.703118']),
                   'apikey': 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'}
 
     map_api_server = "https://static-maps.yandex.ru/v1"
     response = requests.get(map_api_server, params=map_params)
-    im = BytesIO(response.content)
-    opened_image = Image.open(im)
-    return opened_image
+    im = response.content
+    with open('map.png', 'wb') as f:
+        f.write(im)
+    return 'map.png'
