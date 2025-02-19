3eeb7adc6e578cd5c62a29c82f2f5c138143050f
kravage <kostyash2009@mail.ru>
2025-02-19 17:45:19 +0300

    working 2nd task

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt6.QtGui import QPixmap
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap, QKeyEvent
 from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel
 from map_func import scale
 from os import remove
@@ -8,19 +10,33 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         label = QLabel(self)
+        self.spn = ['0.002', '0.002']
 
-        self.img = scale()
+        self.img, self.spn = scale(self.spn)
 
         pixmap = QPixmap(self.img)
         label.setPixmap(pixmap)
         self.setCentralWidget(label)
         self.resize(pixmap.width(), pixmap.height())
 
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key.Key_PageUp:
+            self.img, self.spn = scale(self.spn, change=True, dir='up')
+        elif e.key() == Qt.Key.Key_PageDown:
+            self.img, self.spn = scale(self.spn, change=True, dir='down')
+        else:
+            super().keyPressEvent(e)
+
     def closeEvent(self):
         remove(self.img)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = MainWindow()
     w.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
--- map_func.py
+++ map_func.py
@@ -2,9 +2,13 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-
-def scale():
-    map_params = {'spn': ','.join(['0.002', '0.002']),
+def scale(now_spn, change=False, dir=None):
+    if change:
+        if dir == 'up':
+            now_spn = [str(float(now_spn[0]) - 0.001), str(float(now_spn[1]) - 0.001)]
+        else:
+            now_spn = [str(float(now_spn[0]) + 0.001), str(float(now_spn[1]) + 0.001)]
+    map_params = {'spn': ','.join(now_spn),
                   'll': ','.join(['37.530887', '55.703118']),
                   'apikey': 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'}
 
@@ -13,4 +17,4 @@ def scale():
     im = response.content
     with open('map.png', 'wb') as f:
         f.write(im)
-    return 'map.png'
+    return ('map.png', now_spn)
